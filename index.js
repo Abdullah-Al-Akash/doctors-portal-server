a9ca959d9bda69bd22b1f0c9b7ea6d5ecd52e548
Abdullah Al Akash <aabdullahalakash@gmail.com>
2021-11-08 00:18:23 +0600

    Update Database and Collection

--- index.js
+++ index.js
@@ -9,11 +9,33 @@ const app = express();
 
 // Middleware:
 app.use(cors());
+app.use(express.json());
 
 // Connect With MongoDB Atlas:
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.7gqmj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
-console.log(uri)
+
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+// Run Function:
+async function run() {
+        try {
+                await client.connect();
+                console.log('Database connected successfully')
+
+                // Create Database and Collection:
+                const database = client.db('doctors_portal');
+                const appointmentCollections = database.collection('appointment');
+
+                // Insert Single Appointment:
+                app.post('/appointment', async (req, res) => {
+
+                })
+        }
+        finally {
+                // await client.close();
+        }
+}
+run().catch(console.dir)
 
 // Get Method:
 app.get('/', (req, res) => {
